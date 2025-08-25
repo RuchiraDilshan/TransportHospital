d97d7b754d4026c1f6b1ec83fb2e337b16062fe0
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-08-25 14:26:51 +0530

    create status detail model

---
+++ TransportHospital/src/main/java/com/example/TransportHospital/models/status_details.java
@@ -0,0 +1,89 @@
+package com.example.TransportHospital.models;
+
+import com.example.TransportHospital.models.vehicle_detail.vehicletype;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+@Entity
+public class status_details {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long status_id;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_number", referencedColumnName = "vehicel_number", nullable = false)
+    private vehicle_detail vehicle_number;
+
+    @Enumerated(EnumType.STRING)
+    private vehicle_detail.vehicletype vehicle_type;
+
+    @Enumerated(EnumType.STRING)
+    private vehiclestatus status;
+
+    public enum vehiclestatus {
+        RUNNING,
+        REPAIR
+    }
+
+    @PrePersist
+    @PreUpdate
+    private void syncVehicleType() {
+        if (vehicle_number != null)
+            this.vehicle_type = vehicle_number.getType();
+    }
+
+    public status_details() {
+        super();
+    }
+
+    public status_details(Long status_id, vehicle_detail vehicle_number, vehicletype vehicle_type,
+            vehiclestatus status) {
+        super();
+        this.status_id = status_id;
+        this.vehicle_number = vehicle_number;
+        this.vehicle_type = vehicle_type;
+        this.status = status;
+    }
+
+    public Long getStatus_id() {
+        return status_id;
+    }
+
+    public void setStatus_id(Long status_id) {
+        this.status_id = status_id;
+    }
+
+    public vehicle_detail getVehicle_number() {
+        return vehicle_number;
+    }
+
+    public void setVehicle_number(vehicle_detail vehicle_number) {
+        this.vehicle_number = vehicle_number;
+    }
+
+    public vehicle_detail.vehicletype getVehicle_type() {
+        return vehicle_type;
+    }
+
+    public void setVehicle_type(vehicle_detail.vehicletype vehicle_type) {
+        this.vehicle_type = vehicle_type;
+    }
+
+    public vehiclestatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(vehiclestatus status) {
+        this.status = status;
+    }
+
+}
