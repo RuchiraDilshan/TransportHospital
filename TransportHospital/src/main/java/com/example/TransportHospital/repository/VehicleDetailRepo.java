9cdb2902ba8a1eccc544c69ccdad30a084816b29
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-08-29 12:43:56 +0530

    add method to fsuggest vehicle number in vehicle detail repo

--- TransportHospital/src/main/java/com/example/TransportHospital/repository/VehicleDetailRepo.java
+++ TransportHospital/src/main/java/com/example/TransportHospital/repository/VehicleDetailRepo.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.TransportHospital.models.VehicleDetail;
+import java.util.List;
 
 @Repository
 public interface VehicleDetailRepo extends JpaRepository<VehicleDetail, Long> {
 
     Optional<VehicleDetail> findByVehiclenumber(String vehiclenumber);
+
+    List<VehicleDetail> findByVehiclenumberContainingIgnoreCase(String vehiclenumber);
 
     Optional<VehicleDetail> findByVehicleid(Long vehicleid);
 
