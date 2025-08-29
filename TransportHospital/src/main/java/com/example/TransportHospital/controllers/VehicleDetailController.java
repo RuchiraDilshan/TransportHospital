d3663499f34ac3bcec70c105f4fa4f3ab8ef5db3
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-08-29 14:24:15 +0530

    add vehicle number sugesstion method in vehicle detail conroller

--- TransportHospital/src/main/java/com/example/TransportHospital/controllers/VehicleDetailController.java
+++ TransportHospital/src/main/java/com/example/TransportHospital/controllers/VehicleDetailController.java
@@ -1,5 +1,7 @@
 package com.example.TransportHospital.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import com.example.TransportHospital.models.VehicleDetail;
 import com.example.TransportHospital.service.VehicleDetailService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/vehicles")
@@ -21,7 +24,7 @@ public class VehicleDetailController {
         this.vehicleDetailService = vehicleDetailService;
     }
 
-    @GetMapping("/{vehiclenumber}")
+    @GetMapping("/number/{vehiclenumber}")
     public ResponseEntity<VehicleDetail> getVehicleByNumber(@PathVariable String vehiclenumber) {
         VehicleDetail vehicle = vehicleDetailService.getVehicleByNumber(vehiclenumber);
         if (vehicle == null) {
@@ -30,13 +33,20 @@ public class VehicleDetailController {
         return ResponseEntity.ok(vehicle);
     }
 
-    @GetMapping("/{vehicleid}")
+    @GetMapping("/id/{vehicleid}")
     public ResponseEntity<VehicleDetail> getVehicleById(@PathVariable Long vehicleid) {
         VehicleDetail vehicle = vehicleDetailService.getVehicleById(vehicleid);
         if (vehicle == null) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(vehicle);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<String>> searchvehiclenumber(@RequestParam String query) {
+        List<String> suggestions = vehicleDetailService.getVehicleNumberSuggestions(query);
+        return ResponseEntity.ok(suggestions);
+
     }
 
     @PostMapping
