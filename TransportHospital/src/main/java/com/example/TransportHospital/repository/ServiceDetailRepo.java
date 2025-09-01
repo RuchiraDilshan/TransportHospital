a30a08296e2edf385791bbbb0316a7bf0d4f0826
Ruchira Dilshan Wijerathna <ruchiradilshan17@gmail.com>
2025-09-01 10:29:49 +0530

    create service cdetail service and update service detail repo

--- TransportHospital/src/main/java/com/example/TransportHospital/repository/ServiceDetailRepo.java
+++ TransportHospital/src/main/java/com/example/TransportHospital/repository/ServiceDetailRepo.java
@@ -1,5 +1,6 @@
 package com.example.TransportHospital.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +13,9 @@ public interface ServiceDetailRepo extends JpaRepository<ServiceDetail, String> 
     Optional<ServiceDetail> findByServiceid(Long serviceid);
 
     List<ServiceDetail> findByVehiclenumber(String vehiclenumber);
+
+    List<ServiceDetail> findByNextservicedateAfter(LocalDate date);
+
+    List<ServiceDetail> findByNextservicedateBefore(LocalDate date);
 
 }
